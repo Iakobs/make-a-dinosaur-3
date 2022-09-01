40f989fc5639af1d0b1bcf1997681d165ea84366
Jacob Ibáñez Sánchez <jacobibanez@jacobibanez.com>
2022-09-01 11:51:15 +0200

    Test with final dino

---
+++ assets/scripts/stesugusaurus_v5.gd
@@ -0,0 +1,24 @@
+extends Spatial
+
+var is_original: bool
+var origin : Material = $peppermint_body.get_surface_material(0)
+export var destination: Material
+
+func _ready():
+	is_original = true
+
+func change_material():
+	if is_original:
+		$AnimationPlayer.play("transition")
+	else:
+		$peppermint_body.set_surface_material(0, origin)
+		is_original = true
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		change_material()
+
+
+func _on_animation_finished(anim_name):
+	$peppermint_body.set_surface_material(0, destination)
+	is_original = false
